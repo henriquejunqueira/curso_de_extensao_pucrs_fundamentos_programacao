eb584ef525329295a9587e0b681aa721b950ccb1
henriquejunqueira <henriquejunqueira15@gmail.com>
2024-04-07 15:25:59 -0300

    atualizando repositorio

---
+++ aula06-operadores-aritmeticos/app.js
@@ -0,0 +1,42 @@
+console.log("Exemplo de operadores aritméticos");
+let a = 5;
+let b = 2;
+
+// Operadores aritiméticos
+let soma = a + b;
+console.log(soma);
+
+let subtracao = a - b;
+console.log(subtracao);
+
+let multiplicacao = a * b;
+console.log(multiplicacao);
+
+let divisao = a / b;
+console.log(divisao);
+
+let modulo = a % b;
+console.log(modulo);
+
+// Operadores incremento e decremento
+let incremento = 3;
+incremento++;
+console.log(incremento);
+
+let decremento = 7;
+decremento--;
+console.log(decremento);
+
+// Operadores de atribuição
+let x = 10;
+x += 5;
+console.log(x);
+
+x -= 3;
+console.log(x);
+
+x *= 2;
+console.log(x);
+
+x /= 4;
+console.log(x);
