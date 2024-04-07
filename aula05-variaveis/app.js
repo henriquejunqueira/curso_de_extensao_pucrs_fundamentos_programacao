e3fb6fb11be119f78a3f132287b9d17943dc9b5f
henriquejunqueira <henriquejunqueira15@gmail.com>
2024-04-07 14:53:47 -0300

    primeiro commit

---
+++ aula05-variaveis/app.js
@@ -0,0 +1,24 @@
+// Variável string
+let nome;
+nome =  "João";
+
+// Variáveis número inteiro
+let idade;
+idade = 25;
+
+let a, b, c;
+a = 1;
+b = 2;
+c = 3;
+
+// Variável booleana
+let statusLogico
+statusLogico = true;
+
+// Exibindo no console
+console.log(idade);
+console.log(nome);
+console.log(statusLogico);
+
+let soma = a + b + c;
+console.log(soma);
